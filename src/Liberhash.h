119a48bb4563b3fd7aaa0a7e671b483b24c91bff
Libercoin Developers Team <30723170+LiberCoinDevTeam@users.noreply.github.com>
2017-11-29 22:21:56 +0700

    Initial commit

---
+++ src/Liberhash.h
@@ -0,0 +1,15 @@
+#ifndef LIBERHASH_H
+#define LIBERHASH_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void liberhshre_hash(const char* input, char* output);
+void liberhshre2_hash(const char* input, char* output);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
